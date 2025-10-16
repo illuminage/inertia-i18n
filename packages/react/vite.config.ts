ebccbc73fc01ad0900fec64dc3d399aabfe3799e
Vlad Svidskiy <svidskiy@illuminati.co.il>
2025-10-16 17:04:22 +0300

    feat(init): set up initial project structure with React
    Add basic configuration files for TypeScript, Vite, and ESLint.
    Include initial source files and dependencies for a React project.
    This establishes the foundation for further development and feature
    implementation.

---
+++ packages/react/vite.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import {resolve} from 'path'
+
+export default defineConfig({
+    plugins: [
+        react(),
+        dts({
+            insertTypesEntry: true,
+        }),
+    ],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            formats: ['es', 'cjs']
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+        }
+    },
+})
